674de2dc4adfed7a9659419303b716603232de67
Jacob Goodall <jacobgoodall@Jacobs-Air.attlocal.net>
2018-12-13 19:01:57 -0600

    email header

--- functions.js
+++ functions.js
@@ -11,8 +11,8 @@ exports.contactEmail = (name, email, phone, quantity, message, option1, option2,
     });
     var mailOptions = {
       from: 'fusionsens@gener8.net',
-      to: 'fusionsens@gener8.net', //jakesclients@gmail.com
-      subject: 'You have a new contact request',
+      to: 'jraygoodall@gmail.com', //jakesclients@gmail.com
+      subject: `You have a new contact request from ${email}`,
       html: `<h4>name:</h4> ${name} <h4>email:</h4> ${email} <h4>phone:</h4> ${phone} <h4>quantity:</h4> ${quantity} <h4>message:</h4> ${message} <h4>Option 1:</h4> ${option1} <h4>Option 2:</h4> ${option2} <h4>Option 3:</h4> ${option3}`,
     };
     transporter.sendMail(mailOptions, function (error, info) {
